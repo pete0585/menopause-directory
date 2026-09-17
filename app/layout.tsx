a966df40a95a84556447e3f577478ec1e84c8f91
Adam Peters <adam@thestrategicveteran.com>
2026-09-16 20:10:55 -0400

    Apply AIdam branding, Studio Zero favicon, and live Ads intelligence

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
